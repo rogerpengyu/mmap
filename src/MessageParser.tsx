6fb660cab395b695b4987d7b205ac3ac7e95f7e7
Roger (Peng) Yu <rogeryu@microsoft.com>
2023-06-02 14:46:28 -0700

    +1

---
+++ src/MessageParser.tsx
@@ -0,0 +1,16 @@
+// MessageParser starter code
+class MessageParser {
+    actionProvider: any;
+    state: any;
+
+    constructor(actionProvider: any, state: any) {
+        this.actionProvider = actionProvider;
+        this.state = state;
+    }
+
+    parse(message: any) {
+        console.log(message)
+    }
+}
+
+export default MessageParser;
